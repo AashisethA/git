0542fa8ebd09c988044b0334454a2e67bb4af27b
Seth <aashi.seth@capgemini.com>
2024-09-12 22:48:22 +0530

    adding

---
+++ demo/src/main/java/com/example/demo/DemoanddemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoanddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoanddemoApplication.class, args);
+	}
+
+}
